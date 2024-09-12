a88990beb7ebe0e6d70a8de5fa8c748c85b69cd7
soumya-99 <soumyadeepmondal1234@gmail.com>
2024-09-12 19:01:52 +0530

    Submit button restored

--- src/Components/BtnComp.js
+++ src/Components/BtnComp.js
@@ -21,14 +21,23 @@ function BtnComp({
 	return (
 		<div className="flex justify-center">
 			{mode == "A" && (
-				<button
-					type="reset"
-					className="inline-flex items-center px-5 py-2.5 mt-4 mr-2 sm:mt-6 text-sm font-medium text-center text-gray-800 border border-yellow-300 bg-yellow-300 transition ease-in-out hover:bg-yellow-200 duration-300 rounded-full  dark:focus:ring-primary-900"
-					onClick={onReset}
-				>
-					<ReloadOutlined className="mr-2" />
-					Reset
-				</button>
+				<>
+					<button
+						type="reset"
+						className="inline-flex items-center px-5 py-2.5 mt-4 mr-2 sm:mt-6 text-sm font-medium text-center text-gray-800 border border-yellow-300 bg-yellow-300 transition ease-in-out hover:bg-yellow-200 duration-300 rounded-full  dark:focus:ring-primary-900"
+						onClick={onReset}
+					>
+						<ReloadOutlined className="mr-2" />
+						Reset
+					</button>
+					<button
+						type="submit"
+						className=" disabled:bg-gray-400 disabled:dark:bg-gray-400 inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-[#6457A6] transition ease-in-out hover:bg-[#4e4480] duration-300  rounded-full focus:ring-gray-600  dark:focus:ring-primary-900 dark:bg-[#92140C] dark:hover:bg-gray-600"
+					>
+						<SaveOutlined className="mr-2" />
+						Submit
+					</button>
+				</>
 			)}
 			{mode == "E" && (
 				<button
