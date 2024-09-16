173a38523eb48bf377d183ac3fc5857fbf2dfe03
Sayantika Dhar <sayantika.dhar04@gmail.com>
2024-09-16 13:24:42 +0530

    reject 15.09.2024

--- router/brn_manager_api.js
+++ router/brn_manager_api.js
@@ -110,7 +110,7 @@ brn_managerRouter.get("/brn_mng_reject_list", async (req, res) => {
   var data = req.query;
 
   var select = `a.*,b.*,d.branch_name,e.loan_type loan_type_name,e.sl_no loan_type,(SELECT CONCAT(a.first_name, ' ', a.last_name) FROM md_users a, td_forward b 
-  WHERE a.id = b.forwarded_by AND a.user_type = '5' AND b.application_no = '${data.application_no}') as forward_appr_name`,
+  WHERE a.id = b.forwarded_by AND a.user_type = '5' AND b.application_no = '${data.application_no}') as forward_appr_name,(SELECT b.remarks FROM td_forward b WHERE b.forwarded_by = '${data.user_id}') as reject_remarks`,
 table_name = 'td_loan_application a, td_forward b, md_branch d, md_loan_type e',
 whr =  `a.application_no = b.application_no 
    AND a.branch_code = d.sl_no
