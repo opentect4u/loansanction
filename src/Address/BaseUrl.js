6c79128e5516715544f97c356ea2b7c4936a3bf7
soumya-99 <soumyadeepmondal1234@gmail.com>
2024-09-19 17:07:32 +0530

    UAT

--- src/Address/BaseUrl.js
+++ src/Address/BaseUrl.js
@@ -1,6 +1,6 @@
 // export const url='https://192.168.1.227:3011'
 
 // export const url='https://apinextgen.opentech4u.co.in:3011'
-export const url = "http://192.168.1.238:3000"
+// export const url = "http://192.168.1.238:3000"
 // export const url = "http://192.168.1.239:3000"
-// export const url = "http://202.21.38.178:3012"
+export const url = "http://202.21.38.178:3012"
